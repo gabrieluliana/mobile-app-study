8beb398e68a5d1f37434b221677c6dac2830ed2a
Gazo <ga.uliana@gmail.com>
2025-02-10 20:21:44 -0300

    setting initial Tailwind

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Aora</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: 'blue'}}> Go to profile</Link>
+    </View>
+  );
+}
